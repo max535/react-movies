989b25816b25b67fd758e6f31e5a6bd4fc9855b4
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-05-29 17:16:27 +0300

    Подключает компоненты Footer и Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Footer } from "./layout/Footer";
 import { Header } from "./layout/Header";
+import { Main } from "./layout/Main";
 
 export function App() {
     return (
         <>
             <Header />
+            <Main />
+            <Footer />
         </>
     );
 }
