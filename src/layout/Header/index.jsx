1379112420b6486b3f354ca40e9f9f0bd1316700
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-05-29 17:14:12 +0300

    Добавляет класс для блока nav

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,6 +1,6 @@
 export function Header() {
     return (
-        <nav>
+        <nav className="green darken-1">
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">React Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
