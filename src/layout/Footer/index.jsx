9ae79f16848244d2b7f1ffdec409357c625938a9
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-05-29 20:20:06 +0300

    Refactoring Footer

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -2,11 +2,11 @@ export function Footer() {
     return (
         <footer className="page-footer green darken-1">
             <div className="footer-copyright">
-              <div className="container">
-              © {new Date().getFullYear()} Copyright Text
-              <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
-              </div>
+                <div className="container">
+                    &copy; {new Date().getFullYear()} Copyright Text
+                    <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                </div>
             </div>
-      </footer>
+        </footer>
     );
 }
