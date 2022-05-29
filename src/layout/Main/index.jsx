dfd6f5b1c42f0e5e3e326d66ccd9cc2ef3656b8e
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-05-29 20:27:16 +0300

    Подключает API OMDB для запроса фильмов

--- src/layout/Main/index.jsx
+++ src/layout/Main/index.jsx
@@ -1,7 +1,31 @@
-export function Main() {
-    return (
-        <main className="container content">
-            Hello from Movies App
-        </main>
-    );
+import React from 'react';
+
+import { Movies } from '../../components/Movies';
+
+export default class Main extends React.Component {
+    state = {
+        movies: [],
+    };
+
+    componentDidMount() {
+        fetch("http://www.omdbapi.com/?apikey=d3632ed&s=matrix")
+          .then(response => response.json())
+          .then(data => this.setState({movies: data.Search}))
+      }
+
+    render() {
+        const {movies} = this.state;
+
+        return (
+            <main className="container content">
+                {
+                    movies.length ? (
+                        <Movies movies={this.state.movies} />
+                    ) : (
+                        <h3>Loading...</h3>
+                    )
+                }
+            </main>
+        );
+    }
 }
